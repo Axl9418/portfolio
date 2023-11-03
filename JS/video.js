188ec2e5648d6da3ef510563a88c83de90b55b6b
Axl9418 <axlmonreal@gmail.com>
2023-11-03 14:43:01 -0700

    Fixing video player

--- JS/video.js
+++ JS/video.js
@@ -1,30 +1,28 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var video = document.getElementById('gameplay-video');
-    var playButton = document.querySelector('.custom-play-button');
-
-    function toggleVideo() {
-        if (video.paused) {
-            video.play();
-            playButton.style.display = 'none'; // Hide the play button
-        } else {
-            video.pause();
-            playButton.style.display = 'block'; // Show the play button
-        }
-    }
-
-    playButton.addEventListener('click', toggleVideo);
-
-    video.addEventListener('mouseover', function() {
-        if (!video.paused) {
-            video.pause();
-            playButton.style.display = 'block'; // Show the play button on hover
-        }
-    });
-
-    video.addEventListener('mouseout', function() {
-        if (!video.paused) {
-            video.play();
-            playButton.style.display = 'none'; // Hide the play button when not hovering
-        }
-    });
-});
+var myFP = fluidPlayer("video", {
+    layoutControls: {
+      controlBar: {
+        autoHideTimeout: 3,
+        animated: true,
+        autoHide: true,
+      },
+      htmlOnPauseBlock: {
+        html: null,
+        height: null,
+        width: null,
+      },
+      autoPlay: false,
+      mute: true,
+      allowTheatre: true,
+      playPauseAnimation: true,
+      playbackRateEnabled: true,
+      allowDownload: true,
+      playButtonShowing: true,
+      fillToContainer: false,
+      posterImage: "images/jumpguy.jpg",
+    },
+    vastOptions: {
+      adList: [],
+      adCTAText: false,
+      adCTATextPosition: "",
+    },
+  });
